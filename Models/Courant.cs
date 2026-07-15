82f4bbd17f0a316099d4eee675e50c350fc11435
robinPecheurBstorm <robin.pecheur@bstorm.be>
2026-07-15 16:32:44 +0200

    Correction Exercice sur les classes

---
+++ Models/Courant.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+	public class Courant
+	{
+		public string Numero { get; set; }
+		private double solde = 0;
+
+		public double Solde
+		{
+			get	
+			{ 
+				return solde; 
+			}
+			private set 
+			{
+				if (value >= - LigneDeCredit)
+				{
+					solde = value; 
+				}
+			}
+		}
+
+		private double ligneDeCredit;
+
+		public double LigneDeCredit
+		{
+			get 
+			{ 
+				return ligneDeCredit; 
+			}
+			set 
+			{
+				if (value >= 0)
+				{
+					ligneDeCredit = value; 
+				}
+			}
+		}
+
+		public Personne Titulaire { get; set; }
+
+		public void Retrait(double montant)
+		{
+			Solde -= montant;
+		}
+		public void Depot(double montant)
+		{
+			Solde += montant;
+		}
+
+
+	}
+}
