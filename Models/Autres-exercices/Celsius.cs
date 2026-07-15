e6d5b90b1560ce67444a4f8676b4f4baa91b91a3
robinPecheurBstorm <robin.pecheur@bstorm.be>
2026-07-15 16:34:21 +0200

    Correction exercice 1 sur surchage opérateur

---
+++ Models/Autres-exercices/Celsius.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models.Autres_exercices
+{
+	public class Celsius
+	{
+		public float Degre { get; set; }
+
+		public static implicit operator Fahrenheit(Celsius celsius)
+		{
+			Fahrenheit fahrenheit = new Fahrenheit();
+			fahrenheit.Degre = (celsius.Degre * 9 / 5) + 32;
+			return fahrenheit;
+		}
+	}
+}
---
+++ Models/Autres-exercices/Fahrenheit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models.Autres_exercices
+{
+	public class Fahrenheit
+	{
+		public float Degre { get; set; }
+
+
+
+		public static explicit operator Celsius(Fahrenheit fahrenheit)
+		{
+			Celsius celsius = new Celsius();
+			celsius.Degre = (fahrenheit.Degre - 32) / 9 * 5;
+			return celsius;
+		}
+	}
+}
